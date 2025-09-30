16e7175815553453f69b6f4b617dd19aef734d17
Kaynen Pellegrino <kaynenbpellegrino@sybertnetics.com>
2025-09-30 11:36:15 -0400

    WE ARE LIVE, BABY!

--- bootstrap/v0.0.7.5/fix_mains.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /mnt/d/SybertneticsUmbrella/SybertneticsAISolutions/MonoRepo/runa/bootstrap/v0.0.7.5/stage1
-for obj in string_utils.o hashtable.o containers.o lexer.o parser.o codegen.o; do
-    objcopy --redefine-sym main=_unused_main_${obj} ${obj}
-done
-echo "Renamed conflicting main symbols"
